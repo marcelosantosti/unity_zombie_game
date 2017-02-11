b36c4b34dd9da10453cc0823a61063efa70bf08c
Marcelo Santos <marcelosantos@MacBook-Pro-de-Marcelo.local>
2017-02-11 12:27:22 -0200

    Adding movement to characters. - Select characters using arrows. - Add force to push charaters back.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject selectedZombie;
+	public List<GameObject> listZombie;
+
+	public Vector3 selectedZombieScale;
+	public Vector3 unselectedZombieScale;
+
+	private int selectedZombieIndex;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (this.selectedZombie != null)
+		{
+			this.SelectZombie();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.GetNewZombieIndex();
+		this.PushZombieUp();
+	}
+
+	private void GetNewZombieIndex()
+	{
+		int newSelectedZombieIndex = this.selectedZombieIndex;
+
+		if (Input.GetKeyDown(KeyCode.RightArrow) == true)
+		{
+			if (newSelectedZombieIndex + 1 == this.listZombie.Count)
+			{
+				newSelectedZombieIndex = 0;
+			}
+			else
+			{
+				newSelectedZombieIndex = newSelectedZombieIndex + 1;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
+		{
+			if (newSelectedZombieIndex == 0)
+			{
+				newSelectedZombieIndex = this.listZombie.Count - 1;
+			}
+			else
+			{
+				newSelectedZombieIndex = this.selectedZombieIndex - 1;
+			}
+		}
+
+		if (this.selectedZombieIndex != newSelectedZombieIndex)
+		{
+			this.UnselectZombie();
+			this.selectedZombieIndex = newSelectedZombieIndex;
+			this.selectedZombie = this.listZombie[this.selectedZombieIndex];
+			this.SelectZombie();
+		}
+	}
+
+	private void PushZombieUp()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow) == true)
+		{
+			Rigidbody rigidBody = this.selectedZombie.GetComponent<Rigidbody>();
+			rigidBody.AddForce(new Vector3(0, 0, 10f), ForceMode.Impulse);
+		}
+	}
+
+	private void SelectZombie()
+	{
+		this.selectedZombie.transform.localScale = this.selectedZombieScale;
+	}
+
+	private void UnselectZombie()
+	{
+		this.selectedZombie.transform.localScale = this.unselectedZombieScale;
+	}
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour 
+{
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	private void AddRigidbody()
+	{
+		//this.rigidbody = base.ad
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
